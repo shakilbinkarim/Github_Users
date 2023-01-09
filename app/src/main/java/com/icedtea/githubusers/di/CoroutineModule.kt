a3662ba890712fff6d6689b108699ecfb4a146bc
SHAKIL BIN KARIM <karimshakilbin@gmail.com>
2023-01-10 03:54:32 +0900

    add coroutine module for di for di

---
+++ app/src/main/java/com/icedtea/githubusers/di/CoroutineModule.kt
@@ -0,0 +1,25 @@
+package com.icedtea.githubusers.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutinesModule {
+
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
