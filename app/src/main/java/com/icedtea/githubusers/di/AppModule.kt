ab7319ab38b744ca277b315b2b094cb1b1d55c79
SHAKIL BIN KARIM <karimshakilbin@gmail.com>
2023-01-12 07:24:24 +0900

    add user service

--- app/src/main/java/com/icedtea/githubusers/di/AppModule.kt
+++ app/src/main/java/com/icedtea/githubusers/di/AppModule.kt
@@ -2,18 +2,26 @@ package com.icedtea.githubusers.di
 
 import com.icedtea.githubusers.BuildConfig
 import com.icedtea.githubusers.data.login.LoginService
+import com.icedtea.githubusers.data.users.UsersService
+import com.icedtea.githubusers.domain.PreferenceStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
+import javax.inject.Inject
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -29,20 +37,63 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideLoginService(): LoginService {
-        return Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
-            .client(
-                OkHttpClient.Builder()
+    fun provideLoginService(): LoginService = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                }
+            ).build()
+        )
+        .build()
+        .create()
+
+    @Provides
+    @Singleton
+    fun provideUserService(): UsersService = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_USER_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
                 .addInterceptor(
                     HttpLoggingInterceptor().apply {
                         level = HttpLoggingInterceptor.Level.BASIC
                     }
                 ).build()
-            )
-            .build()
-            .create()
+        )
+        .build()
+        .create()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        headerInterceptor: HeaderInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            addInterceptor(headerInterceptor)
+        }.build()
+    }
+
+    @Singleton
+    class HeaderInterceptor @Inject constructor(
+        private val preferenceStorage: PreferenceStorage
+    ) : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response = runBlocking {
+            val original = chain.request()
+            val originalHttpUrl = original.url
+            val token = preferenceStorage.accessToken.first()
+            val requestBuilder = original.newBuilder()
+                .addHeader(
+                    "Authorization",
+                    "Bearer $token"
+                )
+                .url(originalHttpUrl)
+            val request = requestBuilder.build()
+            return@runBlocking chain.proceed(request)
+        }
     }
 
 }
