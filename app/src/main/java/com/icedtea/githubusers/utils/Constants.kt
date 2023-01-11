3f867192b9f8364a200e88d897f457e37e9065bf
SHAKIL BIN KARIM <karimshakilbin@gmail.com>
2023-01-12 05:27:09 +0900

    add flow sub time

--- app/src/main/java/com/icedtea/githubusers/utils/Constants.kt
+++ app/src/main/java/com/icedtea/githubusers/utils/Constants.kt
@@ -2,3 +2,5 @@ package com.icedtea.githubusers.utils
 
 const val AUTH_URL = "https://github.com/login/oauth/" +
         "authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URL"
+
+const val FLOW_SUBSCRIPTION_TIME = 5000L
