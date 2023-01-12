8875b680ef5d72e0390ea3bc772f11db99f09507
Shakil <shakil.karim@unifa-e.com>
2023-01-12 17:16:02 +0900

    rm keys

--- app/src/main/java/com/icedtea/githubusers/utils/ClientData.kt
+++ app/src/main/java/com/icedtea/githubusers/utils/ClientData.kt
@@ -1,6 +1,6 @@
 package com.icedtea.githubusers.utils
 
-const val CLIENT_ID = "af1f1e37a0b4a7add181"
-const val CLIENT_SECRET = "64d6d2ef1283563ea192dddf70cd238c7dce3bff"
-const val REDIRECT_URL = "icedtea.io://testcb"
+const val CLIENT_ID = "" // Add your client id here
+const val CLIENT_SECRET = "" // Add your client secret here
+const val REDIRECT_URL = "" // Add your redirect url here
 
