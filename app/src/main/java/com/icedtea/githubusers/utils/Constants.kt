f1ce6c302e205bf871f675e0702ddd01235fdb29
SHAKIL BIN KARIM <karimshakilbin@gmail.com>
2023-01-15 16:20:52 +0900

    add network bound resources utility calss

--- app/src/main/java/com/icedtea/githubusers/utils/Constants.kt
+++ app/src/main/java/com/icedtea/githubusers/utils/Constants.kt
@@ -6,3 +6,5 @@ const val AUTH_URL = "https://github.com/login/oauth/" +
 const val FLOW_SUBSCRIPTION_TIME = 5000L
 
 const val DEFAULT_PAGE_SIZE = 20
+
+const val UNKNOWN_ERROR = "Unknown Error Occurred"
---
+++ app/src/main/java/com/icedtea/githubusers/utils/NetworkBoundResource.kt
@@ -0,0 +1,66 @@
+package com.icedtea.githubusers.utils
+
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.internal.NopCollector.emit
+import retrofit2.Response
+
+/**
+ * A generic class that can provide a resource backed by both
+ * the SQLite database and the network.
+ *
+ * [ResultType] represents the type for database.
+ * [RequestType] represents the type for network.
+ */
+abstract class NetworkBoundResource<ResultType : Any, RequestType> {
+
+    fun asFlow(): Flow<Either<String, ResultType>> = flow {
+        val dbValue = loadFromDb().first()
+        if (shouldFetch(dbValue)) {
+            emit(Either.Right(dbValue))
+            val apiResponse = fetchFromNetwork()
+            when {
+                apiResponse.isSuccessful -> {
+                    apiResponse.body()?.let { saveNetworkResult(it) }
+                    emitAll(loadFromDb().map { Either.Right(it) })
+                }
+                else -> {
+                    onFetchFailed()
+                    emit(
+                        Either.Left(
+                            apiResponse.errorBody()?.string() ?: UNKNOWN_ERROR
+                        )
+                    )
+                }
+            }
+        } else {
+            emitAll(loadFromDb().map { Either.Right(it) })
+        }
+    }.catch { e ->
+        // emit errors as well as any saved data from the DB
+        emit(
+            Either.Left(
+                e.message ?: UNKNOWN_ERROR
+            )
+        )
+        emitAll(loadFromDb().map { Either.Right(it) })
+    }
+
+    /**
+     * useful for operations like retrying the request
+     */
+    protected open fun onFetchFailed() {}
+
+    @WorkerThread
+    protected abstract suspend fun saveNetworkResult(result: RequestType)
+
+    @MainThread
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    @MainThread
+    protected abstract fun loadFromDb(): Flow<ResultType>
+
+    @MainThread
+    protected abstract suspend fun fetchFromNetwork(): Response<RequestType>
+}
